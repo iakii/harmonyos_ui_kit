af0caa6fea3b4a020d583574b04d102428e5635e
Kai.Xiong <xiongkai@petalmail.com>
2026-06-24 14:27:39 +0800

    feat: add staggered grid view implementation with sliver support
    - Introduced SliverVariableSizeBoxAdaptorWidget and SliverVariableSizeBoxAdaptorElement for lazy loading of variable size children.
    - Implemented SliverStaggeredGrid for a two-dimensional staggered layout.
    - Created StaggeredGrid and StaggeredGridView widgets for easier usage.
    - Added StaggeredTile class to define tile dimensions in the grid.
    - Exported necessary files for staggered grid view functionality.

---
+++ lib/widgets/staggered_grid_view/src/rendering/rendering.dart
@@ -0,0 +1,4 @@
+export 'sliver_staggered_grid.dart';
+export 'sliver_variable_size_box_adaptor.dart';
+export 'staggered_grid.dart';
+export 'tile_container_render_object_mixin.dart';
---
+++ lib/widgets/staggered_grid_view/src/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'sliver.dart';
+export 'staggered_grid.dart';
+export 'staggered_grid_view.dart';
+export 'staggered_tile.dart';
